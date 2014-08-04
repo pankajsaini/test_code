db88d002bcc9cfdfe1516c9877e8b5a310e32585
pankaj saini <saini.info@gmail.com>
2014-08-04 15:20:26 +0530

    ete

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -2,14 +2,14 @@ require "zendesk_auth"
 class AuthorizationsController < ApplicationController
 
   def new
-    if params[:subdomain].blank? && params[:identifier].blank? && params[:secret].blank?
-      redirect_to return_message("error","required_parameters_missing") and return
-    else
+    if params[:subdomain] && params[:identifier] && params[:secret]
       session[:subdomain] = params[:subdomain]
       session[:identifier] = params[:identifier]
       session[:secret] = params[:secret]
       auth_obj = ZendeskAuth.new(session[:subdomain],session[:identifier],session[:secret],authorizations_get_access_token_url)
       redirect_to auth_obj.get_request_auth_code_url
+    else
+      redirect_to return_message("error","required_parameters_missing") and return
     end
   end
 
@@ -20,17 +20,21 @@ class AuthorizationsController < ApplicationController
       auth_obj = ZendeskAuth.new(session[:subdomain],session[:identifier],session[:secret],authorizations_get_access_token_url)
       auth_obj.code = params[:code]
       response = auth_obj.get_access_token
-      unless response.empty?
-        authentication_key= AuthenticationKey.authenticate_subdomain(session[:subdomain])
-        store_access_token(authentication_key,response)
-        redirect_to return_message("success","authenticated_successfully") and return
-      else
-        redirect_to return_message("error","access_token_not_found") and return
-      end
+      get_access_token_by_auth_code(response)
     else
       redirect_to return_message("error","auth_code_not_found") and return
     end
 
+  end
+
+  def get_access_token_by_auth_code(response)
+    unless response.empty?
+      authentication_key= AuthenticationKey.authenticate_subdomain(session[:subdomain])
+      store_access_token(authentication_key,response)
+      redirect_to return_message("success","authenticated_successfully") and return
+    else
+      redirect_to return_message("error","access_token_not_found") and return
+    end
   end
 
 

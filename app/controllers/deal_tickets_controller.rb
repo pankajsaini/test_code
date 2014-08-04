9a26502b7c223501b2a6cb18acbd41e9fed66329
pankaj saini <saini.info@gmail.com>
2014-08-04 19:32:52 +0530

    zxcxzx

--- app/controllers/deal_tickets_controller.rb
+++ app/controllers/deal_tickets_controller.rb
@@ -16,11 +16,20 @@ class DealTicketsController < ApplicationController
     if people_associated_deal["status"] == 200
       associated_people =  pipeline_obj.get_associated_people(people_associated_deal["result"])
       authentication_key= AuthenticationKey.authenticate_subdomain(session[:subdomain])
+      deal_tickets(authentication_key,associated_people)
+    else
+      render :json => exception_message(people_associated_deal["message"],people_associated_deal["status"],people_associated_deal["error"]).to_json and return
+    end
+  end
+
+  def deal_tickets(authentication_key,associated_people)
+    if authentication_key
       zendeskApi_obj = Zendesk.new(authentication_key.subdomain, authentication_key.access_token, authentication_key.token_type)
       render :json => tickets(associated_people,zendeskApi_obj).to_json  , :status => 200 and return
     else
-     render :json => exception_message(people_associated_deal["message"],people_associated_deal["status"],people_associated_deal["error"]).to_json and return
+      render :json => exception_message('Not Found',404,"Access Token not found").to_json and return
     end
+
   end
 
  

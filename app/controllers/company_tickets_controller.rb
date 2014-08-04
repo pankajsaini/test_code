e4b624620c834159803d9e075e13b2b363fa0ba6
pankaj saini <saini.info@gmail.com>
2014-08-04 16:23:08 +0530

    test

--- app/controllers/company_tickets_controller.rb
+++ app/controllers/company_tickets_controller.rb
@@ -6,11 +6,8 @@ class CompanyTicketsController < ApplicationController
     session[:subdomain] = params[:subdomain]
     company_id = params[:company_id]
     api_key = params[:api_key]
-
     pipeline_obj = PipelineDeals.new(api_key)
-    pipeline_obj.company_id = company_id
-    people_associated_company = pipeline_obj.get_company_details
-
+    people_associated_company = get_company_details(pipeline_obj,api_key,company_id)
     if people_associated_company["status"] == 200
       associated_people =  pipeline_obj.get_associated_people(people_associated_company["result"])
       get_company_tickets(associated_people)
@@ -23,9 +20,17 @@ class CompanyTicketsController < ApplicationController
 
 
   def get_company_tickets(associated_people)
-    unless associated_people.blank?
+    if associated_people
       authentication_key= AuthenticationKey.authenticate_subdomain(session[:subdomain])
       zendeskApi_obj = Zendesk.new(authentication_key.subdomain, authentication_key.access_token, authentication_key.token_type)
+      render :json => get_tickets(associated_people,zendeskApi_obj).to_json  , :status => 200 and return
+    else
+      exception_obj = ExceptionMessage.new("Not Found",404,"email address of associated people not found on pipelinedeals")
+      render :json => exception_obj.message.to_json and return
+    end
+  end
+
+  def get_tickets(associated_people,zendeskApi_obj)
       data_array = []
       associated_people.each {|email|
         zendeskApi_obj.person_email = email
@@ -35,11 +40,12 @@ class CompanyTicketsController < ApplicationController
         response["tickets"] =nil
         data_array <<  zendeskApi_obj.get_person_tickets(person_obj,response)
       }
-      render :json => data_array.to_json  , :status => 200 and return
-    else
-      exception_obj = ExceptionMessage.new("Not Found",404,"email address of associated people not found on pipelinedeals")
-      render :json => exception_obj.message.to_json and return
-    end
+    return data_array
+  end
+
+  def get_company_details(pipeline_obj,api_key,company_id)
+    pipeline_obj.company_id = company_id
+    return pipeline_obj.get_company_details
   end
 
 
